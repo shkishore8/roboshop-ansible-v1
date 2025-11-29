763dce98b0aa34b5eba8c6de1f9b04f5e46e34fe
kishore babu <shkishore8@gmail.com>
2025-11-29 08:49:53 +0530

    roboshop

--- run.sh
+++
@@ -1,6 +0,0 @@
-git pull
-if [ -z "$1" ]; then
-  echo Input is expected
-  exit 1
-fi
-ansible-playbook -i ${1}-dev.shkishore.online, ${1}.yml -e ansible_user=ec2-user -e ansible_password=DevOps321
